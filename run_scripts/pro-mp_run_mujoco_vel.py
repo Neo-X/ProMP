0c17ffe9f134f77dfb2edcf32ac3caa75e5467b2
Zhiwei-Z <zhiweizhang2012@gmail.com>
2020-01-01 21:05:19 -0800

    fix bug

--- run_scripts/pro-mp_run_mujoco_vel.py
+++ run_scripts/pro-mp_run_mujoco_vel.py
@@ -88,7 +88,7 @@ def main(config):
 
     with tf.Session().as_default() as sess:
         with open('./saved_policies/mjvel.policy', 'wb') as policy_file:
-            pickle.dump(self.policy, policy_file)
+            pickle.dump(policy, policy_file)
             print("saved policies")
 
     trainer.train()
@@ -139,7 +139,7 @@ if __name__=="__main__":
             'target_inner_step': 0.01,
             'init_inner_kl_penalty': 5e-4,
             'adaptive_inner_kl_penalty': False, # whether to use an adaptive or fixed KL-penalty coefficient
-            'n_itr': 0, # number of overall training iterations
+            'n_itr': 100, # number of overall training iterations
             'meta_batch_size': 40, # number of sampled meta-tasks per iterations
             'num_inner_grad_steps': 1, # number of inner / adaptation gradient steps
 
